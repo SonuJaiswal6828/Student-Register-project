b97c42fb67adcc4a9b6bfcb8e92e15ce53fcd4ef
Sonu Jaiswal <jsonu77768@gmail.com>
2026-08-04 21:12:13 +0530

    Update CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -10,15 +10,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-origins = [
-    "http://127.0.0.1:5500",  # Local development
-    "http://localhost:5500",
-    "https://student-register-project.vercel.app", 
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

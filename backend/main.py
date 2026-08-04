f8b85567fa3d05a8d455c04948104899c1038927
Sonu Jaiswal <jsonu77768@gmail.com>
2026-08-04 21:25:59 +0530

    Update CORS middleware to limit allowed origins
    Restrict CORS origins to only the production URL.

--- backend/main.py
+++ backend/main.py
@@ -9,15 +9,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "https://student-register-project.vercel.app",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "https://student-register-project.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

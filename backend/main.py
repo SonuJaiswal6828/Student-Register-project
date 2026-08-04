272a5dc84162ba5a6094a8d9c10bb50061146a9a
Sonu Jaiswal <jsonu77768@gmail.com>
2026-08-04 21:13:45 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,15 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://student-register-project.vercel.app",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
